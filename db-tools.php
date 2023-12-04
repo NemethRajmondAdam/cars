78650b2053b715f892e1b65141b446723ee639f3
3041TAN-17 <3041TAN-17@boronkay.local>
2023-12-04 08:45:08 +0100

    adding createMaker function

---
+++ db-tools.php
@@ -0,0 +1,17 @@
+<?php
+
+function createMaker($mysqli,$maker)
+{
+    $result = $mysqli->query("INSERT INTO makers (name) VALUES ('$maker')");
+    if(!$result){
+        echo "Hiba történt a $maker beszúrása közben";
+    }
+
+    return $result;
+}
+
+/*function updateMaker($mysqli,$data)
+{
+    $result = $mysqli
+}*/
+?>
