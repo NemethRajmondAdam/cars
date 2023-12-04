2578208976e4adc2e66d25af03fb52716c711e96
3041TAN-17 <3041TAN-17@boronkay.local>
2023-12-04 08:21:45 +0100

    asd

--- cars.php
+++ cars.php
@@ -1,36 +1,70 @@
 <?php
-$array = [];
+
+require_once('csv-tools.php');
+ini_set('memory_limit', '-1');
+
+$filename = "car-db.csv";
+$csvData = getCsvData($filename);
+if (empty($csvData)) {
+    echo 'A fájl nem található';
+    return false;
+}
+
+/*
+$header = $csvData[0];
+$idxMaker = array_search('make', $header);
+$idxModel = array_search('model', $header);
+
+$isHeader = true;
+
 $result = [];
-$file = "car-db.csv";
-ini_set('memory_limit', '1000M');
-
-if (file_exists($file)) {
-    $csvFile = fopen($file,"r");
-    while (!feof($csvFile)) {
-        $line = fgetcsv($csvFile);
-        $array[] = $line;
-        //print_r($data);
+$maker = '';
+$model = '';
+foreach ($csvData as $data) {
+    if (!is_array($data)) {
+        continue;
+    }
+    if ($isHeader) 
+    {
+        $isHeader = false;
+        continue;    
+    }
+    if ($maker != $data[$idxMaker]) {
+        $maker = $data[$idxMaker];
+    }
+    if ($model != $data[$idxModel]) {
+        $model = $data[$idxModel];
+        $result[$maker][] = $model;
     }
 }
-$sv = count($array);
-$maker = "";
-$model = "";
-for ($i=0; $i < $sv; $i++) { 
-    if ($maker != $array[$i][1]) {
-        $maker = $array[$i][1];
-    }
-    if ($model != $array[$i][2]) {
-        $result[$maker][]=$model;
-    }
-    /*$result[$i][0]=$array[$i][1];
-    $result[$i][1]=$array[$i][2];*/
-}
-//print_r($array);
 print_r($result);
+*/
 
 
 
+$mysqli = new mysqli("localhost","root",null,"cars");
+//check connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL " . $mysqli -> connect_errno;
+    exit();
+}
 
+echo "connected\n";
+
+
+$makers = getMakers($csvData);
+
+print_r($makers);
+
+
+
+foreach ($makers as $maker) {
+    $mysqli->query("INSERT INTO cars (name) VALUES ($maker)");
+    echo "$maker\n";
+}
+
+
+$mysqli->close();
 
 
 ?>
